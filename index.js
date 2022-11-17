d6e20db55dc20cbee9d5d1d9f25491a60f20b574
NabilWasir <89061107+NabilWasir@users.noreply.github.com>
2022-11-18 00:37:20 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector("#hambergermenu");
+const menu = document.querySelector("#menu");
+const closeBtn = document.querySelector(".overlay-hambergermenu");
+
+btn.addEventListener("click", function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
+
+closeBtn.addEventListener("click", function () {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
